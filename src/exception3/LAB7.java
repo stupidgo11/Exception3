b9516fa1b07eb2f62d8f3fee1eeaf3a1ebea4a4b
Natthaphol <zerozkiller@gmail.com>
2023-03-28 16:29:51 +0700

    lab finished

---
+++ src/exception3/LAB7.java
@@ -0,0 +1,22 @@
+package exception3;
+
+public class LAB7 {
+    public static void main(String[] args) {
+        System.out.println("Main method call method doThis()");
+        try{
+            doThis();
+        }
+        catch(Exception e){
+            System.out.println("Main method receive exception "+e.getMessage());
+        }
+    }
+
+    public static void doThis() throws Exception{
+        System.out.println("Method doThis() call method doThat()");
+        doThat();
+    }
+    public static void doThat() throws Exception{
+        throw new MyException("Hello MyException");
+        //System.out.println(1/0);
+    }
+}
---
+++ src/exception3/MyException.java
@@ -0,0 +1,12 @@
+package exception3;
+
+public class MyException extends Exception{
+    
+    public MyException(String msg){
+        super(msg);
+    }
+    
+    public MyException(){
+        super();
+    }
+}
